6862fd1c7a7bb90b62e24a851d64045d50c39e98
coolboymu <35949725+coolboymu@users.noreply.github.com>
2018-06-01 00:23:32 +0800

    Add files via upload

---
+++ ball_in_box/ballinbox.py
@@ -0,0 +1,59 @@
+import math
+import random
+
+
+__all__ = ['ball_in_box']
+
+def ball_in_box(m, blockers):
+    k=0
+    max=0
+    circles = []
+    BalloonR=[0]*int(m)
+    BalloonXPos=[0]*int(m)
+    BalloonYPos=[0]*int(m)
+
+    while k<50000:
+        sum=0
+        BalloonR=[0]*int(m)
+        for j in range(0,int(m)):
+            r=0
+            BalloonXPos[j]=random.random()*2-1
+            BalloonYPos[j]=random.random()*2-1
+            i=0
+            while(i<j):
+                if(math.sqrt((BalloonXPos[j]-BalloonXPos[i])**2+(BalloonYPos[j]-BalloonYPos[i])**2)-BalloonR[i]>0):
+                    i=i+1
+                else:
+                    i=0
+                    BalloonXPos[j]=random.random()*2-1
+                    BalloonYPos[j]=random.random()*2-1
+                    continue
+    
+            r=math.fabs(1-BalloonXPos[j])
+            if(math.fabs(1-BalloonYPos[j])<r):
+                r=math.fabs(1-BalloonYPos[j])
+            if(math.fabs(-1-BalloonXPos[j])<r):
+                r=math.fabs(-1-BalloonXPos[j])
+            if(math.fabs(-1-BalloonYPos[j])<r):
+                r=math.fabs(-1-BalloonYPos[j])
+            if(math.sqrt((BalloonXPos[j]-0.5)**2+(BalloonYPos[j]-0.5)**2)<r):
+                r=math.sqrt((BalloonXPos[j]-0.5)**2+(BalloonYPos[j]-0.5)**2)
+            if(math.sqrt((BalloonXPos[j]-0.5)**2+(BalloonYPos[j]+0.3)**2)<r):
+                r=math.sqrt((BalloonXPos[j]-0.5)**2+(BalloonYPos[j]+0.3)**2)
+            for i in range(0,j):
+                if(math.sqrt((BalloonXPos[j]-BalloonXPos[i])**2+(BalloonYPos[j]-BalloonYPos[i])**2)-BalloonR[i]<r):
+                    r=math.sqrt((BalloonXPos[j]-BalloonXPos[i])**2+(BalloonYPos[j]-BalloonYPos[i])**2)-BalloonR[i]
+            BalloonR[j]=r
+            sum=sum+BalloonR[j]**2
+        if(sum>max):
+            max=sum
+        k=k+1
+    for circle_index in range(m):
+
+        x = BalloonXPos[circle_index]
+        y = BalloonYPos[circle_index]
+        r = BalloonR[circle_index]
+        circles.append((x, y, r))
+        circle_index += 1
+    
+    return circles

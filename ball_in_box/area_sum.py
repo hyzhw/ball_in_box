d4ef3a5e3ddd5e89e92da39ba7e11e0379f9fbd5
hyzhw <39647600+hyzhw@users.noreply.github.com>
2018-06-01 23:22:50 +0800

    Update area_sum.py

--- ball_in_box/area_sum.py
+++ ball_in_box/area_sum.py
@@ -1,6 +1,6 @@
 import math
 import ball_in_box.ballinbox as bb
-
+import ball_in_box.validate as val
 
 def area_sum(circles):
     area = 0.0
@@ -12,16 +12,15 @@ def area_sum(circles):
 if __name__ == '__main__':
     num_of_circle = 5
     blockers = [(0.5, 0.5)
-               ,(0.5, -0.3)]     #障碍物坐标及大小
+               ,(0.5, -0.3)]
     
     circles = bb.ball_in_box(num_of_circle, blockers)
     
-    if num_of_circle == len(circles) :
+    if num_of_circle == len(circles) and val.validate(circles, blockers):
         area = area_sum(circles)
         print("Total area: {}".format(area))
     else:
         print("Error: no good circles.")
-
 
 
 
